fd7b64840cdb35dc970a781c57995c26050c3ece
shabuddinshaik <mr.shabuddinshaik@gmail.com>
2023-09-09 13:45:20 +0530

    Added config files for application logs

---
+++ BookStore/src/main/java/com/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookstoreApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BookstoreApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Starting the Bookstore application...");
+
+        // Your application logic
+
+        logger.info("Shutting down the Bookstore application...");
+    }
+}
